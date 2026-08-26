70e7e772141a1d82e207394e683f7b10ff2e0a82
KenRits <is0768es@ed.ritsumei.ac.jp>
2026-08-26 14:22:50 +0900

    main.pyにコードexampleを作成.

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
-import nn
-import function as f
-from graph import Graph
 
-Variable = f.Variable
-Parameter = f.Parameter
+from nn import StandardMLP
 
 import numpy as np
+from matplotlib import pyplot as plt
 
-# Variable(1, array([[-2.12692828],
-#        [-5.33969204]]))
+# モデル定義. ユニットの数は, 入力層1, 中間層16 ×2層, 出力層1
+model = StandardMLP([1, 16, 16, 1], "r")
 
-loss_func = f.MSELoss()
-X = Variable(np.array([[1, 1, 3]]))
-Y = Variable(np.array([[1, 2, 3]]))
-loss = loss_func(X, Y, axis=0)
-print(loss)
-g = loss.backward(Graph())
-g.show()
-print(X.grad)
+# 訓練データ作成
+rng = np.random.default_rng(0)
+
+# y = x**2 + noise というデータを100個, X \in [-5, 5]の範囲で作成
+data_n = 100
+X = np.linspace(-5, 5, data_n)
+y_train = X**2 + rng.normal(0, 1, data_n)
+
+# 訓練データの可視化
+plt.scatter(X, y_train, color="blue", label="Training data")
+
+# 学習
+model.fit(X, y_train)
+
+# 予測
+prediction = model.predict(X)
+
+# 予測の可視化
+plt.plot(X, prediction, color="red", label="MLP's prediction")
+plt.legend()
+plt.grid()
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
+
+
