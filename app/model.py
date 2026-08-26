0a063980ea41c85005243787201192507f049e24
KenRits <is0768es@ed.ritsumei.ac.jp>
2026-08-26 21:31:37 +0900

    無駄なコメントアウトを削除

--- app/model.py
+++ app/model.py
@@ -1,18 +1,6 @@
 import numpy as np
 
 rng = np.random.default_rng(0)
-
-# class Model:
-#     def __init__(self, X, y):
-#         if X.shape[0] != y.shape[0]:
-#             raise exception.InvalidDataShapeException(f"X.shape[0]: {X.shape[0]}, y.shape[0]: {y.shape[0]}")
-                
-#         self.X = X
-#         self.y = y
-#         self.labels = np.unique(y)
-#         self.n = X.shape[0]
-#         self.d = X.shape[1]
-
 class Model:
     def __init__(self):
         pass
