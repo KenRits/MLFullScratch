fb4a08808a2fcbe3b47134656b054340850259b6
KenRits <is0768es@ed.ritsumei.ac.jp>
2026-08-26 14:23:47 +0900

    nn.modelにpredictメソッドを追加し, 0次元, 1次元の引数を2次元のnp.ndarrayに変換する, model._to_2dメソッドを追加.

--- nn.py
+++ nn.py
@@ -79,11 +79,38 @@ class Model:
         raise NotImplementedError("Model.fitは継承先でのみ使用できます.")
 
     def _sample(self, X_train: np.ndarray, y_train: np.ndarray, batch_size: int) -> tuple[Variable]:
-        indices = rng.choice(N, batch_size)
+        indices = rng.choice(len(X_train), batch_size)
         X_batch = Variable(X_train[indices].T)
         y_batch = Variable(y_train[indices].T)
 
         return X_batch, y_batch
+
+    def _to_2d(self, X: np.ndarray) -> np.ndarray:
+        """0次元のnp.ndarray(int, float型含む), もしくは1次元のnp.ndarrayを2次元の縦ベクトルに変換する. 
+
+        Args:
+            X (np.ndarray): 2次元以下のnp.ndarray, int, float, list. 
+
+        Raises:
+            ValueError: 入力が3次元以上, もしくはnp.ndarray, int, float, list以外の型だった場合に発火.
+
+        Returns:
+            np.ndarray: 縦ベクトルとなったX. Xが2次元であった場合, そのままXが返される.
+        """
+        if isinstance(X, list):
+            X = np.array(X)
+
+        if isinstance(X, int) or isinstance(X, float):
+            return np.atleast_2d(X)
+        
+        if X.ndim < 2:
+            return np.atleast_2d(X).T
+        
+        elif X.ndim == 2:
+            return X
+        
+        else:
+            raise ValueError(f"モデルの入力は2次元以下のnp.ndarrayか, int, floatに限られます. type(X): {type(X)}")
 
 class NeuralNetwork(Model):
     def __init__(self, layers):
@@ -136,7 +163,7 @@ class StandardMLP(NeuralNetwork):
             raise ValueError(f"X_train, y_trainの長さを揃えてださい. len(X_train): {len(X_train)}, len(y_train): {len(y_train)}")
 
         if batch_size is None:
-            batch_size = min(10, len(X))
+            batch_size = min(10, len(X_train))
 
         optimizer = opt.Adam(self.params)
 
@@ -163,6 +190,13 @@ class StandardMLP(NeuralNetwork):
             painter.animate()
 
         return loss_record
+
+    def predict(self, X: np.ndarray):
+        X = self._to_2d(X)
+        X_var = Variable(X.T)
+        out = self.forward(X_var)
+        return out.data.T
+
 
 
 
